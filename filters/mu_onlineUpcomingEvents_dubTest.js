bbebc6b1cc9aa1b23cf9eee0b1b6998d85266898
Adarsh Chakraborty <adarsh.chakraborty@mastersunion.org>
2024-02-19 10:02:01 +0530

    restructure

--- filters/mu_onlineUpcomingEvents_dubTest.js
+++ filters/mu_onlineUpcomingEvents_dubTest.js
@@ -1,60 +1,61 @@
-
-
-
 const fs = require("fs");
 let dublicateEvents = 0;
 
 async function getPastEvents() {
-    const upcomingEventSet = new Set();
-    const allEvents = [];
-  
-    let currentPage = 1;
-    let totalPages = Infinity; 
-   
-  
-    while (currentPage <= totalPages) {
-        console.log("Sending request for page: ", currentPage);
-      const response = await fetch(`http://localhost:32000/api/getUpcomingEventsMastersunion?page=${currentPage}&eLoc=Online`);
-      const responseData = await response.json();
-  
-      if (!responseData.IsSuccess) {
-        console.error("Error fetching events:", responseData.Message);
-        return;
-      }
+	const upcomingEventSet = new Set();
+	const allEvents = [];
 
-      
-  
-      const { upcomingEvents, totalPages: total, currentPage: page } = responseData.Data;
-  
-      totalPages = total;
-      console.log("[RES] totalPages: ", total, " on page: ", currentPage);
-  
-      // Check for duplicate events
-      upcomingEvents.forEach(event => {
-        if (upcomingEventSet.has(event._id)) {
-          console.log("Duplicate event found:", event._id);
-          dublicateEvents++;
-        } else {
-          upcomingEventSet.add(event._id);
-          allEvents.push(event);
-        }
-      });
-  
-      currentPage++;
-    }
-  
-    return allEvents;
-  }
-  
-  // Usage
-  getPastEvents()
-    .then(allEvents => {
-        console.log("Total Events: ", allEvents.length);
-        console.log("Dublicate Events: ", dublicateEvents);
-        fs.writeFileSync("mu_onlineUpcomingEvents_dubTest.json", JSON.stringify(allEvents));
-      
-    })
-    .catch(error => {
-      console.error("Error:", error);
-    });
-  
+	let currentPage = 1;
+	let totalPages = Infinity;
+
+	while (currentPage <= totalPages) {
+		console.log("Sending request for page: ", currentPage);
+		const response = await fetch(
+			`http://localhost:32000/api/getUpcomingEventsMastersunion?page=${currentPage}&eLoc=Online`
+		);
+		const responseData = await response.json();
+
+		if (!responseData.IsSuccess) {
+			console.error("Error fetching events:", responseData.Message);
+			return;
+		}
+
+		const {
+			upcomingEvents,
+			totalPages: total,
+			currentPage: page
+		} = responseData.Data;
+
+		totalPages = total;
+		console.log("[RES] totalPages: ", total, " on page: ", currentPage);
+
+		// Check for duplicate events
+		upcomingEvents.forEach((event) => {
+			if (upcomingEventSet.has(event._id)) {
+				console.log("Duplicate event found:", event._id);
+				dublicateEvents++;
+			} else {
+				upcomingEventSet.add(event._id);
+				allEvents.push(event);
+			}
+		});
+
+		currentPage++;
+	}
+
+	return allEvents;
+}
+
+// Usage
+getPastEvents()
+	.then((allEvents) => {
+		console.log("Total Events: ", allEvents.length);
+		console.log("Dublicate Events: ", dublicateEvents);
+		fs.writeFileSync(
+			"./json/mu_onlineUpcomingEvents_dubTest.json",
+			JSON.stringify(allEvents)
+		);
+	})
+	.catch((error) => {
+		console.error("Error:", error);
+	});
